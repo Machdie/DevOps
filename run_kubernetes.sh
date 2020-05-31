ba411939ed8845bad9f0272ff23c0bdfe3746cff
EC2 Default User <ec2-user@ip-172-31-38-20.eu-central-1.compute.internal>
2020-05-31 14:26:38 +0000

    adding files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=machdinho/devops
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run machdie-devops 
+--image=machdinho/devops 
+--port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward machdie-devops 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,13 +6,16 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath=machdinho/api
+dockerpath=machdinho/devops
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+export DOCKER_ID_USER="machdinho"
+docker login
+docker tag machdinho/devops $DOCKER_ID_USER/devops 
 
 # Step 3:
-# Push image to a docker repository
-docker push $dockerpath
 
+# Push image to a docker repository
+docker push $DOCKER_ID_USER/devops 
